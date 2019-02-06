1fe1810be13e31ec0b800430ee61fc4c68e38bb5
Matthew Wilson <wilsonmatt219@gmail.com>
2019-02-06 19:02:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+function getFirstSelector(selector) {
+ return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  var x = document.getElementById("app").querySelectorAll("ul.ranked-list")
+  for (let i = 0; i < x.length; i++) {
+  x[i].innerHTML = ((parseInt(x[i].innerHTML)) + n);
+}
+}
+
+function deepestChild() {
+  var x = document.getElementById("grand-node").querySelectorAll("div")
+  return x[x.length -1];
+}
