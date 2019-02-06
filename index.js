7e118a69efabca1f0eb40df31c72f294c537caba
Matthew Wilson <wilsonmatt219@gmail.com>
2019-02-06 18:34:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   var x = document.querySelectorAll("ranked-list")
   for (let i = 0; i < x.length; i++;)
-  x[i].innerHTML = ((parseInt))
+  x[i].innerHTML = ((parseInt)x[i])
 }
 
 function deepestChild() {
