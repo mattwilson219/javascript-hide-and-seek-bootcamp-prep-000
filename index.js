1780ab9c4a675e1ed6616efc17c20064d18524aa
Matthew Wilson <wilsonmatt219@gmail.com>
2019-02-06 19:01:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  var x = document.getElementById("grand-node").querySelectorAll()
+  var x = document.getElementById("grand-node").querySelectorAll("div")
+  return []
 }
